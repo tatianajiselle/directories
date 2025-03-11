e9181d2c1d50e529d22407170510aaa4cb9f1c84
Pontus Abrahamsson <pontus@lostisland.co>
2025-03-11 17:09:00 +0100

    wip

--- apps/cursor/src/app/jobs/new/page.tsx
+++ apps/cursor/src/app/jobs/new/page.tsx
@@ -1,4 +1,30 @@
-export default function JobsNew() {
+import { GithubSignin } from "@/components/github-signin";
+import { GoogleSignin } from "@/components/google-signin";
+import { getSession } from "@/utils/supabase/auth";
+
+export default async function JobsNew() {
+  const session = await getSession();
+
+  if (!session) {
+    return (
+      <div className="min-h-screen flex items-center justify-center p-4 w-full max-w-sm mx-auto">
+        <div className="max-w-md w-full text-center -mt-32">
+          <p className="text-md mt-4">
+            Sign in to post a job listing <br />
+            and reach 220k+ developers today.
+          </p>
+
+          <div className="mt-10 flex flex-col gap-4">
+            <div className="flex flex-col gap-4">
+              <GithubSignin redirectTo="/jobs/new" />
+              <GoogleSignin redirectTo="/jobs/new" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-8 mt-10 max-w-screen-sm xl:max-w-screen-md border-t border-border pt-10">
       <h1 className="text-2xl font-bold">Create a new job listing</h1>
--- apps/cursor/src/app/layout.tsx
+++ apps/cursor/src/app/layout.tsx
@@ -65,7 +65,7 @@ export const viewport = {
   maximumScale: 1,
   userScalable: false,
   themeColor: [
-    { media: "(prefers-color-scheme: light)" },
+    // { media: "(prefers-color-scheme: light)" },
     { media: "(prefers-color-scheme: dark)" },
   ],
 };
@@ -81,7 +81,7 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cn(
         `${GeistSans.variable} ${GeistMono.variable}`,
-        "whitespace-pre-line antialiased bg-background text-foreground",
+        "whitespace-pre-line antialiased bg-background text-foreground !dark",
       )}
     >
       <body>
