c70b52db22e9ab41b3c2658058eb2d7f5bd51921
Pontus Abrahamsson <pontus@lostisland.co>
2025-03-15 18:02:43 +0100

    Add install command

--- apps/cursor/src/components/rule-card.tsx
+++ apps/cursor/src/components/rule-card.tsx
@@ -13,6 +13,7 @@ import { CopyButton } from "./copy-button";
 import { InstallCommand } from "./install-command";
 import { SaveRuleButton } from "./save-rule-button";
 import { ShareButton } from "./share-button";
+
 export function RuleCard({ rule, isPage }: { rule: Rule; isPage?: boolean }) {
   return (
     <Card className="bg-background p-4 max-h-[calc(100vh-8rem)] aspect-square flex flex-col">
@@ -23,7 +24,7 @@ export function RuleCard({ rule, isPage }: { rule: Rule; isPage?: boolean }) {
         )}
       >
         <div className="group-hover:flex hidden right-4 bottom-4 absolute z-10 space-x-2">
-          <InstallCommand slug={rule.slug} />
+          {isPage && <InstallCommand slug={rule.slug} />}
           <ShareButton slug={rule.slug} />
           <CopyButton content={rule.content} slug={rule.slug} />
           <SaveRuleButton content={rule.content} slug={rule.slug} />
