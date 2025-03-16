24536899d4226851a00dc3e2b32fb913550f8912
Pontus Abrahamsson <pontus@lostisland.co>
2025-03-16 19:06:08 +0100

    Redirect

--- apps/cursor/src/app/companies/page.tsx
+++ apps/cursor/src/app/companies/page.tsx
@@ -25,7 +25,7 @@ export default async function Page() {
         </div>
 
         <Suspense fallback={null}>
-          <AddCompanyButton />
+          <AddCompanyButton redirect={true} />
         </Suspense>
       </div>
 
